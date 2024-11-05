31d8693fd39b836e07ef5a6794c97c81f21d4b95
Tobias Henning <tobias081996@googlemail.com>
2024-11-05 19:19:21 +0100

    Removed some cs

--- ConsoleApplication1/ConsoleApplication1/ImageToBlur.cpp
+++ ConsoleApplication1/ConsoleApplication1/ImageToBlur.cpp
@@ -3,91 +3,35 @@
 #include "opencv2/opencv.hpp"
 #include "ImageHelper.h"
 
-
-
 /*
  * Function to convert an image
  * @param image The image to convert
- * @param i The row indei
- * @param j The column indei
+ * @param i The row index
+ * @param j The column index
  */
 void convertImageToBlur(cv::Mat image, cv::Mat new_image, int i, int j) {
 
     if (i < 1 || j < 1 || i >= image.rows - 1 || j >= image.cols - 1) {
-        switch (image.channels()) {
-            case 1:
-                new_image.at<uchar>(i, j) = image.at<uchar>(i, j);
-				break;
-			case 3:
-				new_image.at<cv::Vec3b>(i, j) = image.at<cv::Vec3b>(i, j);
-				break;
-			case 4:
-				new_image.at<cv::Vec4b>(i, j) = image.at<cv::Vec4b>(i, j);
-				break;
-			default:
-				throw std::runtime_error("Unsupported number of image channels");
+        for (int k = 0; k < image.channels(); ++k) {
+            new_image.ptr<unsigned char>(i, j)[k] = image.ptr<unsigned char>(i, j)[k];
         }
         return;
     }
 
-    switch (image.channels()) {
-        case 1: { // Grayscale image
-            int sum = image.at<uchar>(i - 1, j + 1) +
-                image.at<uchar>(i, j + 1) +
-                image.at<uchar>(i + 1, j + 1) +
-                image.at<uchar>(i - 1, j) +
-                image.at<uchar>(i, j) +
-                image.at<uchar>(i + 1, j) +
-                image.at<uchar>(i - 1, j - 1) +
-                image.at<uchar>(i, j - 1) +
-                image.at<uchar>(i + 1, j - 1);
+    std::vector<int> sum(image.channels(), 0);
 
-            uchar average = cv::saturate_cast<uchar>(sum / 9);
-            new_image.at<uchar>(i, j) = average;
-            break;
-        }
-        case 3: { // RGB image
-            cv::Vec3i sum = cv::Vec3i(image.at<cv::Vec3b>(i - 1, j + 1)) +
-                cv::Vec3i(image.at<cv::Vec3b>(i, j + 1)) +
-                cv::Vec3i(image.at<cv::Vec3b>(i + 1, j + 1)) +
-                cv::Vec3i(image.at<cv::Vec3b>(i - 1, j)) +
-                cv::Vec3i(image.at<cv::Vec3b>(i, j)) +
-                cv::Vec3i(image.at<cv::Vec3b>(i + 1, j)) +
-                cv::Vec3i(image.at<cv::Vec3b>(i - 1, j - 1)) +
-                cv::Vec3i(image.at<cv::Vec3b>(i, j - 1)) +
-                cv::Vec3i(image.at<cv::Vec3b>(i + 1, j - 1));
+    for (int k = 0; k < image.channels(); ++k) {
+        sum[k] += image.ptr<unsigned char>(i - 1, j + 1)[k];
+        sum[k] += image.ptr<unsigned char>(i, j + 1)[k];
+        sum[k] += image.ptr<unsigned char>(i + 1, j + 1)[k];
+        sum[k] += image.ptr<unsigned char>(i - 1, j)[k];
+        sum[k] += image.ptr<unsigned char>(i, j)[k];
+        sum[k] += image.ptr<unsigned char>(i + 1, j)[k];
+        sum[k] += image.ptr<unsigned char>(i - 1, j - 1)[k];
+        sum[k] += image.ptr<unsigned char>(i, j - 1)[k];
+        sum[k] += image.ptr<unsigned char>(i + 1, j - 1)[k];
 
-            cv::Vec3b average;
-            average[0] = cv::saturate_cast<uchar>(sum[0] / 9);
-            average[1] = cv::saturate_cast<uchar>(sum[1] / 9);
-            average[2] = cv::saturate_cast<uchar>(sum[2] / 9);
-
-            new_image.at<cv::Vec3b>(i, j) = average;
-            break;
-        }
-        case 4: { // RGBA image
-            cv::Vec4i sum = cv::Vec4i(image.at<cv::Vec4b>(i - 1, j + 1)) +
-                cv::Vec4i(image.at<cv::Vec4b>(i, j + 1)) +
-                cv::Vec4i(image.at<cv::Vec4b>(i + 1, j + 1)) +
-                cv::Vec4i(image.at<cv::Vec4b>(i - 1, j)) +
-                cv::Vec4i(image.at<cv::Vec4b>(i, j)) +
-                cv::Vec4i(image.at<cv::Vec4b>(i + 1, j)) +
-                cv::Vec4i(image.at<cv::Vec4b>(i - 1, j - 1)) +
-                cv::Vec4i(image.at<cv::Vec4b>(i, j - 1)) +
-                cv::Vec4i(image.at<cv::Vec4b>(i + 1, j - 1));
-
-            cv::Vec4b average;
-            average[0] = cv::saturate_cast<uchar>(sum[0] / 9);
-            average[1] = cv::saturate_cast<uchar>(sum[1] / 9);
-            average[2] = cv::saturate_cast<uchar>(sum[2] / 9);
-            average[3] = cv::saturate_cast<uchar>(sum[3] / 9);
-
-            new_image.at<cv::Vec4b>(i, j) = average;
-            break;
-        }
-        default:
-            // Handle unexpected number of channels
-            throw std::runtime_error("Unsupported number of image channels");
+        new_image.ptr<unsigned char>(i, j)[k] = sum[k] / 9;
     }
 }
 
